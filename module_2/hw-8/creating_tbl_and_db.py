4ac74b425eefc90e8d3928f90b72379706443bb9
ErihKoh <barsuk0831@gmail.com>
2021-11-11 20:36:58 +0200

    created foo for creating table and db

--- module_2/hw-8/creating_tbl_and_db.py
+++ module_2/hw-8/creating_tbl_and_db.py
@@ -37,8 +37,22 @@ def creating_tbl(conn, db):
                              ON DELETE SET NULL
                              ON UPDATE CASCADE)"""
 
+        teacher_tbl = """CREATE TABLE teachers(id INT PRIMARY KEY NOT NULL,
+                                 last_name TEXT NULL)"""
+
+        course_and_grade_tbl = """CREATE TABLE courses
+                                (id INT NOT NULL,
+                                teacher INT NOT NULL,
+                                student INT NOT NULL,
+                                CONSTRAINT fk_tch_cr FOREIGN KEY (teacher) REFERENCES teachers (id),
+                                CONSTRAINT fk_std_cr FOREIGN KEY (student) REFERENCES students (id),
+                                grade INT NOT NULL,
+                                date DATE NOT NULL)"""
+
         cursor.execute(group_tbl)
         cursor.execute(student_tbl)
+        cursor.execute(teacher_tbl)
+        cursor.execute(course_and_grade_tbl)
         connection.commit()
         print("Successfully created table")
 
