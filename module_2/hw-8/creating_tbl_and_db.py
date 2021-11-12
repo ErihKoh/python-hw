bf03a555e3102a96ac2477f9f3cf543a3e0133e1
ErihKoh <barsuk0831@gmail.com>
2021-11-12 19:03:06 +0200

    fix create db

--- module_2/hw-8/creating_tbl_and_db.py
+++ module_2/hw-8/creating_tbl_and_db.py
@@ -1,5 +1,4 @@
 from psycopg2 import Error, DatabaseError
-from test_data import test_lst, test_list2
 from data import group_lst, std_lst, teacher_lst, courses_lst
 
 
@@ -56,30 +55,11 @@ def creating_tbl(conn, db):
                                         grade INT NOT NULL,
                                         date DATE NOT NULL)"""
 
-        # course_and_grade_tbl = """CREATE TABLE courses
-        #                         (id INT PRIMARY KEY NOT NULL,
-        #                         course_name VARCHAR(128),
-        #                         teacher INT NOT NULL,
-        #                         CONSTRAINT fk_tch_cr FOREIGN KEY (teacher) REFERENCES teachers (id),
-        #                         grade INT NOT NULL,
-        #                         date DATE NOT NULL)"""
-
-        # courses_and_students = """CREATE TABLE std_crs
-        #                           (id INT NOT NULL,
-        #                           student_id INT NOT NULL,
-        #                           course_id INT NOT NULL,
-        #                           CONSTRAINT fk_std_cr_std FOREIGN KEY (student_id) REFERENCES students (id)
-        #                               ON DELETE SET NULL
-        #                               ON UPDATE CASCADE,
-        #                           CONSTRAINT fk_std_cr_cr FOREIGN KEY (course_id) REFERENCES courses (id)
-        #                               ON DELETE SET NULL
-        #                               ON UPDATE CASCADE)"""
-
         cursor.execute(group_tbl)
         cursor.execute(student_tbl)
         cursor.execute(teacher_tbl)
         cursor.execute(course_and_grade_tbl)
-        # cursor.execute(courses_and_students)
+
         connection.commit()
         print("Successfully created tables")
 
@@ -132,25 +112,13 @@ def helper_insert_teacher():
 def helper_insert_courses():
     insert_query = "INSERT INTO courses (id, course_name, teacher, student, grade, date) VALUES "
 
-    for i in test_lst:
+    for i in courses_lst:
 
-        if i == test_lst[-1]:
+        if i == courses_lst[-1]:
             insert_query += f" {i}"
         else:
             insert_query += f" {i},"
     return insert_query
-
-
-# def helper_insert_courses_and_std():
-#     insert_query = "INSERT INTO std_crs (id, student_id, course_id) VALUES "
-#
-#     for i in test_list2:
-#
-#         if i == test_list2[-1]:
-#             insert_query += f" {i}"
-#         else:
-#             insert_query += f" {i},"
-#     return insert_query
 
 
 def insert_data(conn, db):
@@ -162,13 +130,11 @@ def insert_data(conn, db):
         students_query = helper_insert_students()
         teacher_query = helper_insert_teacher()
         courses_query = helper_insert_courses()
-        # courses_std_query = helper_insert_courses_and_std()
 
         cursor.execute(group_query)
         cursor.execute(students_query)
         cursor.execute(teacher_query)
         cursor.execute(courses_query)
-        # cursor.execute(courses_std_query)
 
         connection.commit()
 
