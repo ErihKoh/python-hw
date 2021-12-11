5dc72a45a6335131504d2e802a7292e3e86b8b16
ErihKoh <barsuk0831@gmail.com>
2021-12-11 19:48:05 +0200

    lru has been completed

--- module_2/hw-10/lru/lru.py
+++ module_2/hw-10/lru/lru.py
@@ -1,39 +1,60 @@
 import redis
 
-r = redis.StrictRedis(host='localhost', port='6381', db=0)
-
-r.mset('firstname' 'Jack' 'lastname' 'Stuntman' 'age' '35')
-print(r.keys())
+# r.mset('firstname' 'Jack' 'lastname' 'Stuntman' 'age' '35')
+# print(r.keys())
 
 # r.set('1', 5)
 # g = r.get('1')
 # r.set('1', 'abs')
 # g = r.get('1')
 # print(g.decode('utf-8'))
-# cache = {}
-# queue_list = []
+queue_list = []
 
 
-# def lru(elm):
-#     if len(queue_list) == 0:
-#         queue_list.append(elm)
-#
-#     if elm in queue_list:
-#         queue_list.remove(elm)
-#         queue_list.insert(0, elm)
-#     if len(queue_list) > 4:
-#         queue_list.pop()
-#
-#     queue_list.insert(0, elm)
-#     cache = {idx: i for idx, i in enumerate(queue_list)}
-#     return cache
-#
-#
-# print(lru(1))
-# lru(3)
-# lru(4)
-# lru(5)
-# lru(6)
-# lru(7)
-# print(lru(3))
-# print(lru(1))
+def set_in_redis(elm, client):
+    if elm in queue_list:
+        queue_list.remove(elm)
+        queue_list.insert(0, elm)
+    else:
+        queue_list.insert(0, elm)
+
+    if len(queue_list) > 5:
+        queue_list.pop()
+
+    for idx, i in enumerate(queue_list):
+        client.set(idx, i)
+
+    return print(f'{elm} has been added to redisDB')
+
+
+def get_from_redis(key, client):
+    data = client.get(key).decode('utf-8')
+    queue_list.remove(data)
+    queue_list.insert(0, data)
+
+    for idx, i in enumerate(queue_list):
+        client.set(idx, i)
+
+    return f'key {key}: {data}'
+
+
+def get_all_data_from_redis(client):
+    for i in range(5):
+        print(client.get(i).decode('utf-8'))
+
+
+if __name__ == '__main__':
+    redis_client = redis.StrictRedis(host='localhost', port='6381', db=0)
+
+    set_in_redis('a', redis_client)
+    set_in_redis('b', redis_client)
+    set_in_redis('c', redis_client)
+    set_in_redis('d', redis_client)
+    set_in_redis('e', redis_client)
+    set_in_redis('f', redis_client)
+    set_in_redis('h', redis_client)
+    set_in_redis('hello', redis_client)
+
+    print(get_all_data_from_redis(redis_client))
+    print(get_from_redis('3', redis_client))
+    print(get_all_data_from_redis(redis_client))
